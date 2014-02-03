8a2c75d8a0cb4b8c3da049c4115a5edb5f1d381f
longblonde <pirate.coffee@gmail.com>
2014-02-03 16:22:08 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,17 +2,19 @@
 
 sudo apt-get update
 
-sudo apt-get install python-mysqldb
+sudo apt-get install --yes --force-yes python-mysqldb
 
-sudo apt-get install python-dev
+sudo apt-get install --yes --force-yes python-dev
 
-sudo apt-get install python-rpi.gpio
+sudo apt-get install --yes --force-yes python-rpi.gpio
 
-sudo apt-get install python-smbus
+sudo apt-get install --yes --force-yes python-smbus
 
-sudo apt-get install i2c-tools
+sudo apt-get install --yes --force-yes i2c-tools
 
 sudo nano /etc/modules
 
 sudo nano /etc/modprobe.d/raspi-blacklist.conf
+
+sudo nano /etc/rc.local
 
