00c47b1a5a5fdcd00f7ea60870fc3314c4aa3741
longblonde <pirate.coffee@gmail.com>
2014-02-03 12:38:35 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sudo apt-get update
+
+sudo apt-get install python-mysqldb
+
+sudo apt-get install python-dev
+
+sudo apt-get install python-rpi.gpio
+
+sudo apt-get install python-smbus
+
+sudo apt-get install i2c-tools
+
+sudo nano /etc/modules
+
